afd22c9fc05c8b74dd53450dec5c021734b064c8
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-03-30 14:01:17 +0900

    ステップ1：Enumを作成する。

---
+++ src/main/java/com/example/springtraining/domain/TaskPriority.java
@@ -0,0 +1,22 @@
+package com.example.springtraining.domain;
+
+/**
+ * タスク優先度を表すEnum。
+ */
+public enum TaskPriority {
+
+  /**
+   * 優先度低
+   */
+  LOW,
+
+  /**
+   * 優先度中
+   */
+  MEDIUM,
+
+  /**
+   * 優先度高
+   */
+  HIGH
+}
