428117ab1cba89e274bf20a81b3c93e041a478a2
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-03-30 11:58:47 +0900

    ステップ2：コントローラーで User を Model にセットする。

---
+++ src/main/java/com/example/springtraining/controller/ModelController.java
@@ -0,0 +1,21 @@
+package com.example.springtraining.controller;
+
+import com.example.springtraining.domain.FullName;
+import com.example.springtraining.domain.Profile;
+import com.example.springtraining.domain.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ModelController {
+
+  @GetMapping("/user")
+  public String showUser(Model model) {
+    FullName fullName = new FullName("Hanako", "Yamada");
+    Profile profile = new Profile(fullName, 25);
+    User user = new User("1", profile);
+    model.addAttribute("user", user);
+    return "user";
+  }
+}
