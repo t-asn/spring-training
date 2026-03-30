75f6ce95188874f4eda55cf06976494ae41e5073
SAKURABO-T\tasan <t.asano.sakurabo@gmail.com>
2026-03-30 11:38:26 +0900

    ステップ4：Bean生成の対象範囲を理解する。

---
+++ src/main/java/com/example/springtraining/UnscannedService.java
@@ -0,0 +1,15 @@
+package com.example.springtraining;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UnscannedService {
+
+  private static final Logger log = LoggerFactory.getLogger(UnscannedService.class);
+
+  public UnscannedService() {
+    log.info("UnscannedService を作成して箱に入れました");
+  }
+}
